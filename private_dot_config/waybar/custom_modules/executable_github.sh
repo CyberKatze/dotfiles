ded21d9b71292df8c5d70e032a3263b0e8e1da0f
Mehrdad Shahidi <m.mehrdadshahidi@gmail.com>
2023-02-17 00:04:57 +0330

    🐛 Waybar github and memory module fixed

--- private_dot_config/waybar/custom_modules/executable_github.sh
+++ private_dot_config/waybar/custom_modules/executable_github.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+if [ -f $HOME/.config/gh/notification.token ]; then
+  token=`cat ${HOME}/.config/gh/notification.token`
+  count=`curl -u username:${token} https://api.github.com/notifications`
+  tooltip="Click to open"
 
-token=`cat ${HOME}/.config/gh/notification.token`
-count=`curl -u username:${token} https://api.github.com/notifications | jq '. | length'`
-tooltip="Click to open"
-
-if [[ "$count" != "0" ]]; then
-    echo '{"text":'$count', "tooltip":"'$tooltip'", "class":"$class"}'
+  if [[ "$count" != "0" ]]; then
+      echo '{"text":'$count', "tooltip":"'$tooltip'", "class":"$class"}'
+  fi
 fi
--- private_dot_config/waybar/custom_modules/executable_memory.sh
+++ private_dot_config/waybar/custom_modules/executable_memory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-text=$(free -h | grep Mem | awk '{print $3}'| sed -e 's/\(.*\)G.*/\1/')
+text=$(free -h | grep Mem | awk '{print $3}'| sed -e 's/\(\d*[MG]\).*/\1/')
 tooltip="$text used"
 echo '{"text": "'${text}'", "tooltip": "'$tooltip'", "class": "$class"}'
 
